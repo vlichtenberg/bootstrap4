1a91353dbab880c526a4ae0eb24405c25f65a32a
Vince <vincentlichtenberg@gmail.com>
2016-09-04 12:36:04 +0200

    grunt and npm setup updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,12 +6,15 @@ module.exports = function (grunt) {
     require('load-grunt-tasks')(grunt);
 
     var scripts = [
-        'js/**/*.js'
+        '_js/**/*.js'
+    ];
+    var sass = [
+        '_sass/styles.scss'
     ];
 
-
     var src = {
-        scripts: scripts
+        scripts: scripts,
+        sass: sass
     };
 
     grunt.initConfig(
@@ -19,12 +22,24 @@ module.exports = function (grunt) {
             // ----- Environment
             pkg: grunt.file.readJSON('package.json'),
 
-
-            jshint: {
-                options: {
-                    jshintrc: '.jshintrc'
-                },
-                all: scripts
+            browserSync: {
+                dev: {
+                    bsFiles: {
+                        src : [
+                            './js/*.js',
+                            './css/*.css',
+                            './img/*.*',
+                            './_stubs/*.html'
+                        ]
+                    },
+                    options: {
+                        server: '/../',
+                            watchTask: true, // watch runs after browserSync
+                            port: 8282, // default port is 3000, browserSync admin is on http://localhost:3001/
+                            directory: true, // show directory listing
+                            open: false // don't open the browser automatically
+                    }
+                }
             },
 
             jscs: {
@@ -33,30 +48,60 @@ module.exports = function (grunt) {
                 },
                 dev: {
                     files: {
-                        src: scripts
+                        src: src.scripts
+                    }
+                }
+            },
+
+            jshint: {
+                options: {
+                    jshintrc: '.jshintrc'
+                },
+                all: src.scripts
+            },
+
+            sass: {
+                dev: {
+                    options: {
+                        outputStyle: 'expanded',
+                        sourceMap: true
+                    },
+                    files: {
+                        'css/styles.css': src.sass
+                    }
+                }
+            },
+
+            uglify: {
+                dev: {
+                    options: {
+                        beautify: true, // for debugging
+                        banner: '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd HH:MM") %> */',
+                        mangle: false, // for debugging
+                        sourceMap: true,
+                        sourceMapIncludeSources: true
+                    },
+                    files: {
+                        'js/<%= pkg.name.toLowerCase() %>.js': src.scripts
                     }
                 }
             },
 
             watch: {
+                sass: {
+                    files: ['_sass/**/*.scss'],
+                    tasks: ['sasslint:all', 'sass:dev']
+                },
                 script: {
-                    files: scripts,
-                    tasks: ['jscs', 'jshint'] 
-                }
-            },
-
-            'http-server': {
-                'dev': {
-                    root: '../../',
-                    runInBackground: true
+                    files: ['_js/**/*.js', '_test/**/*.js'],
+                    tasks: ['jscs', 'jshint', 'uglify:dev'] // the :run flag for karma is required
                 }
             }
-
 
         }
     );
 
-    grunt.registerTask('default', ['jscs', 'jshint', 'http-server', 'watch']);
+    grunt.registerTask('default', ['jscs', 'jshint', 'uglify:dev', 'sass:dev', 'browserSync', 'watch']);
     grunt.loadNpmTasks('grunt-notify');
 
 };
